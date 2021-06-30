a62f2b8e96e653bd0bd9beb05456e5dcf111e6aa
okakyo <oka@logly.co.jp>
2021-06-30 17:29:36 +0900

    GraphQL-codegen を追加

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = (api) => {
+	api.cache(true)
+	return {
+	  presets: ['next/babel'],
+	  plugins: [
+		['module-resolver', {
+		  extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+		  alias: {
+			'@': './',
+		  },
+		}],
+	  ],
+	}
+  }
+  
